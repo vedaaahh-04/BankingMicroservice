1608fc6de83c1c8df9b3edb58d9a1ef2e6d62b37
vedaaahh-04 <140682843+vedaaahh-04@users.noreply.github.com>
2024-11-19 19:16:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,69 +4,69 @@ provider "azurerm" {
 }
 
 # Resource Group
-resource "azurerm_resource_group" "gr" {
-  name     = "gr-S2"
+resource "azurerm_resource_group" "rg_new" {
+  name     = "rg-New"
   location = "Canada Central"
 }
 
 # Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-S2"
+resource "azurerm_virtual_network" "vnet_new" {
+  name                = "vnet-New"
   address_space       = ["192.168.0.0/19"]
-  location            = azurerm_resource_group.gr.location
-  resource_group_name = azurerm_resource_group.gr.name
+  location            = azurerm_resource_group.rg_new.location
+  resource_group_name = azurerm_resource_group.rg_new.name
 }
 
 # Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-S2"
-  resource_group_name  = azurerm_resource_group.gr.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+resource "azurerm_subnet" "subnet_new" {
+  name                 = "subnet-New"
+  resource_group_name  = azurerm_resource_group.rg_new.name
+  virtual_network_name = azurerm_virtual_network.vnet_new.name
   address_prefixes     = ["192.168.0.0/24"]
 }
 
 # Network Security Group
-resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-S2"
-  location            = azurerm_resource_group.gr.location
-  resource_group_name = azurerm_resource_group.gr.name
+resource "azurerm_network_security_group" "nsg_new" {
+  name                = "nsg-New"
+  location            = azurerm_resource_group.rg_new.location
+  resource_group_name = azurerm_resource_group.rg_new.name
 }
 
 # Public IP Address
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "vm-public-ip"
-  location            = azurerm_resource_group.gr.location
-  resource_group_name = azurerm_resource_group.gr.name
+resource "azurerm_public_ip" "vm_public_ip_new" {
+  name                = "vm-public-ip-new"
+  location            = azurerm_resource_group.rg_new.location
+  resource_group_name = azurerm_resource_group.rg_new.name
   allocation_method   = "Static"
   sku                  = "Standard"
-  domain_name_label   = "myvm-public-ip"
+  domain_name_label   = "myvm-public-ip-new"
 }
 
 # Network Interface for Linux VM
-resource "azurerm_network_interface" "nic_linux" {
-  name                = "nic-linux-vm"
-  location            = azurerm_resource_group.gr.location
-  resource_group_name = azurerm_resource_group.gr.name
+resource "azurerm_network_interface" "nic_linux_new" {
+  name                = "nic-linux-vm-new"
+  location            = azurerm_resource_group.rg_new.location
+  resource_group_name = azurerm_resource_group.rg_new.name
 
   ip_configuration {
-    name                          = "ipconfig-linux"
-    subnet_id                     = azurerm_subnet.subnet.id
+    name                          = "ipconfig-linux-new"
+    subnet_id                     = azurerm_subnet.subnet_new.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id  # Associate Public IP
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip_new.id  # Associate Public IP
   }
 }
 
 # Linux Virtual Machine
-resource "azurerm_linux_virtual_machine" "linux_vm" {
-  name                            = "linux-vm"
-  resource_group_name             = azurerm_resource_group.gr.name
-  location                        = azurerm_resource_group.gr.location
+resource "azurerm_linux_virtual_machine" "linux_vm_new" {
+  name                            = "linux-vm-new"
+  resource_group_name             = azurerm_resource_group.rg_new.name
+  location                        = azurerm_resource_group.rg_new.location
   size                            = "Standard_DS1_v2"
-  admin_username                  = "adminuser"
+  admin_username                  = "adminuser_new"
   admin_password                  = "Password@123"  # Please consider using a more secure way to handle this, e.g., Key Vault, or environment variables
   disable_password_authentication = false
 
-  network_interface_ids = [azurerm_network_interface.nic_linux.id]
+  network_interface_ids = [azurerm_network_interface.nic_linux_new.id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -90,6 +90,6 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
 }
 
 # Output Public IP Address
-output "vm_public_ip" {
-  value = azurerm_public_ip.vm_public_ip.ip_address
+output "vm_public_ip_new" {
+  value = azurerm_public_ip.vm_public_ip_new.ip_address
 }
